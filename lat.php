74e67184593d28bb4a4f12318deb36ebc2da6597
unknown <19250785@bsi.sc.id>
2026-05-13 08:28:28 +0700

    first commit

---
+++ lat.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post">
+    NIM : <input type="text" name="nim"><br>
+    Nama : <input type="text" name="nama"><br>
+    <button type="submit" name="kirim">Kirim</button>
+</form>
+
+<?php
+if(isset($_POST['kirim'])){
+    echo "NIM : ".$_POST['nim']."<br>";
+    echo "Nama : ".$_POST['nama'];
+}
+?>
+
+</body>
+</html>
